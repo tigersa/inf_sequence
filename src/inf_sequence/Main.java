a6c427de1ab1720531a119d7a778fb8862c16395
a.stepura <a.stepura@rlt.ru>
2015-10-22 14:06:09 +0300

    (*) При подсчете индекса тип данных с long изменен на BigInteger

--- src/inf_sequence/Main.java
+++ src/inf_sequence/Main.java
@@ -1,6 +1,7 @@
 package inf_sequence;
 
 import java.io.File;
+import java.math.BigInteger;
 import java.util.Scanner;
 import java.util.ArrayList;	
 import javax.swing.JOptionPane;
@@ -26,71 +27,6 @@ public class Main {
 		}
 	}
 	
-	public static void main(String args[]){	
-		ArrayList<ArrayList<Byte>> a = new ArrayList<ArrayList<Byte>>();
-		readDataFromFile(a);
-				
-		for (int i = 0; i < a.size(); i++)
-			System.out.println("Number: " + a.get(i) + ", Index in sequence: " + findIndex(a.get(i)));
-	}
-	
-	private static long findIndex(ArrayList<Byte> number) {
-		
-		for(int digit = 1; digit <= number.size(); digit++){
-			for(int start_index = 0; start_index <= number.size() - digit; start_index++){//выделяем из искомой последовательности часть цифр и считаем их числом
-				long index = try_to_find_sequence(number, digit, start_index);  //пытаемся найти слева числа на единицу меньше и справа на единицу больше пока последовательность не законичится или число не будет найдено
-				if (index != -1){
-					return index;
-				}
-			}
-		}
-		return -1;
-	}
-
-	private static long try_to_find_sequence(ArrayList<Byte> number, int digit, int start_index) {
-		ArrayList<Byte> value = new ArrayList<Byte>();
-		for(int i = start_index + digit - 1; i >= start_index; i--) value.add(number.get(i));
-
-		int left_i = start_index - 1, right_i = start_index + digit;
-		
-		ArrayList<Byte> prev_value = value;
-		while(left_i >= 0){
-			prev_value = decrement(prev_value);
-			int count = Math.min(prev_value.size() - 1, left_i) + 1;
-			for(int i = 0; i < count; i++){
-				if(number.get(left_i - i) != prev_value.get(i)){
-					return -1;
-				}
-			}
-			left_i -= count;
-		}
-		
-		ArrayList<Byte> next_value = value;
-		while(right_i < number.size()){
-			next_value = increment(next_value);
-			int count = Math.min(next_value.size() - 1, number.size() - right_i - 1) + 1;
-			for(int i = 0; i < count; i++){
-				if(number.get(right_i + i) != next_value.get(next_value.size() - i - 1)){
-					return -1;
-				}
-			}
-			right_i += count;
-		}
-		
-		//последовательность найдена, считаем индекс
-		long n = 0;
-		for(int i = 0; i < value.size(); i++){
-			n += value.get(i) * Math.pow(10, i);
-		}
-		long index = 0;
-		for(int i = 1; i < value.size(); i++){
-			index += i * (Math.pow(10, i) - Math.pow(10, i - 1));
-		}
-		index += value.size() * (n - Math.pow(10, value.size() - 1)) + 1;
-		return index - start_index;
-	}
-	
-
 	private static ArrayList<Byte> increment(ArrayList<Byte> value) {
 		ArrayList<Byte> new_value = new ArrayList<Byte>();
 		Byte next_degree = 0;
@@ -114,5 +50,78 @@ public class Main {
 		}
 		return new_value;
 	}
+	
+	private static BigInteger findIndex(ArrayList<Byte> number) {
+		
+		for(int digit = 1; digit <= number.size(); digit++){
+			for(int start_index = 0; start_index <= Math.min(number.size() - digit, digit - 1); start_index++){//выделяем из искомой последовательности часть цифр и считаем их числом
+				BigInteger index = try_to_find_sequence(number, digit, start_index);  //пытаемся найти слева числа на единицу меньше и справа на единицу больше пока последовательность не законичится или число не будет найдено
+				if (index.compareTo(BigInteger.valueOf(-1)) != 0){
+					return index;
+				}
+			}
+		}
+		return BigInteger.valueOf(-1);
+	}
 
+	private static BigInteger try_to_find_sequence(ArrayList<Byte> number, int digit, int start_index) {
+		ArrayList<Byte> value = new ArrayList<Byte>();
+		if (number.get(start_index) == 0) return BigInteger.valueOf(-1); //число не может начинаться с нуля
+		for(int i = start_index + digit - 1; i >= start_index; i--) value.add(number.get(i));
+		
+		
+		int left_i = start_index - 1, right_i = start_index + digit;
+		
+		ArrayList<Byte> prev_value = value;
+		while(left_i >= 0){
+			prev_value = decrement(prev_value);
+			int count = Math.min(prev_value.size() - 1, left_i) + 1;
+			for(int i = 0; i < count; i++){
+				if(number.get(left_i - i) != prev_value.get(i)){
+					return BigInteger.valueOf(-1);
+				}
+			}
+			left_i -= count;
+		}
+		
+		ArrayList<Byte> next_value = value;
+		while(right_i < number.size()){
+			next_value = increment(next_value);
+			int count = Math.min(next_value.size() - 1, number.size() - right_i - 1) + 1;
+			for(int i = 0; i < count; i++){
+				if(number.get(right_i + i) != next_value.get(next_value.size() - i - 1)){
+					return BigInteger.valueOf(-1);
+				}
+			}
+			right_i += count;
+		}
+		
+		//последовательность найдена, считаем индекс
+		BigInteger n = BigInteger.valueOf(0);
+		for(int i = 0; i < value.size(); i++){
+			n = n.add(BigInteger.valueOf(value.get(i)).multiply(BigInteger.valueOf(10).pow(i)));
+		}
+		BigInteger index = new BigInteger("0");
+		for(int i = 1; i < value.size(); i++){
+			//index += i * (Math.pow(10, i) - Math.pow(10, i - 1));
+			index = index.add(BigInteger.valueOf(i).multiply(BigInteger.valueOf(10).pow(i).subtract(BigInteger.valueOf(10).pow(i - 1))));
+		}
+		//index += value.size() * (n - Math.pow(10, value.size() - 1)) + 1;
+		index = index.add(BigInteger.valueOf(value.size()).multiply(n.subtract(BigInteger.valueOf(10).pow(value.size() - 1)))).add(BigInteger.valueOf(1));
+		index = index.subtract(BigInteger.valueOf(start_index));
+		return index;
+	}
+	
+	public static void main(String args[]){	
+		ArrayList<ArrayList<Byte>> a = new ArrayList<ArrayList<Byte>>();
+		readDataFromFile(a);
+				
+		for (int i = 0; i < a.size(); i++){
+			String number = "";
+			for (int j = 0; j < a.get(i).size(); j++){
+				number += a.get(i).get(j).toString();
+			}
+			System.out.println("Number: " + number + ", Index in sequence: " + findIndex(a.get(i)));
+		}
+	}
 }
